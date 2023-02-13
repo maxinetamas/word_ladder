7f5ca85644ab50b8a5df4a5cb6db25c5f5bc3ca3
Maxine Tamas <mtamas25@cmc.edu>
2023-02-13 14:02:47 -0800

    word_ladder.py

--- word_ladder.py
+++ word_ladder.py
@@ -28,6 +28,27 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     Whenever it is impossible to generate a word ladder between the two words,
     the function returns `None`.
     '''
+    stack = []
+    stack.push(start_word)
+    queue = deque([])
+    queue = deque([stack])
+    
+    while len(queue) != 0:
+        while queue:
+            curr_stack = queue.popleft()
+            curr_word = curr_stack[-1]
+            for i in range(len(curr_word)):
+                for j in "abcdefghijklmnopqrstuvwxyz":
+                    new_word = curr_word[:i] + j + curr_word[i+1:]
+                    if new_word == end_word:
+                        return curr_stack + [new_word]
+                    stack_copy = curr_stack + [new_word]
+                    queue.append(stack_copy)
+
+    if start_word == end_word:
+        return [start_word]
+
+    return []
 
 
 def verify_word_ladder(ladder):
@@ -40,6 +61,10 @@ def verify_word_ladder(ladder):
     >>> verify_word_ladder(['stone', 'shone', 'phony'])
     False
     '''
+    for i in enumerate(ladder):
+        if adjacent(enumerate[i], enumerate[i + 1]) == False:
+            return False
+    return True 
 
 
 def _adjacent(word1, word2):
@@ -52,3 +77,11 @@ def _adjacent(word1, word2):
     >>> _adjacent('stone','money')
     False
     '''
+    count = 0
+    if len(word1) == len(word2):
+        for i in enumerate(word1):
+            if word1[i] != word2[i]:
+                count += 1
+    if count == 1:
+        return False
+    return True
